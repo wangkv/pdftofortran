1f2b1420d478bb9be17d71cb26633c79bf1e05fa
wangkv <48386437+wangkv@users.noreply.github.com>
2021-05-13 15:05:14 +0800

    Update fortran-1.py

--- fortran-1.py
+++ fortran-1.py
@@ -1,36 +1,32 @@
 def space(lines):
-    letter = ["C", "*"]
-    shuzi = ["1", "2", "3", "4", "+", "5"]
-    shuzi2 = ["10", "20", "30", "40", "50"]
-    zifu = ["CALL", "CHAR"]
+    letter = ["C", "*", "c"]
+    shuzi = ["1", "2", "3", "4", "+", "5", "&", "$"]
+    shuzi2 = [str(i) for i in range(0,100,10)]
+    shuzi3 = [str(i) for i in range(100,1000,10)]
+    shuzi4 = [str(i) for i in range(1000,10000,10)]
+    zifu = ["CALL", "CHAR", "call"]
     yema = ["·"+str(i)+"·" for i in range(100)]
-    # print(yema)
-    # print(yema[0])
     for line in lines:
         line = line.strip(" ")
         if line.split():
-            # file2.write(line)
             if line[0:4] in yema or line[0:3] in yema:
                 continue
             elif line[0:4] in zifu:  # 首字母是C，但需要缩进的代码
                 file2.write("      " + line)
-            elif line[0] in letter:
-                file2.write(line)
+            elif line[0:4] in shuzi4:
+                file2.write(" " + line)
+            elif line[0:3] in shuzi3:
+                file2.write("  " + line)
             elif line[0:2] in shuzi2:  # 缩进三个空格，DO程序的执行范围前的数字
                 file2.write("   " + line)
             elif line[0] in shuzi:  # 缩进五个空格，程序链接的标志
                 file2.write("     " + line)
+            elif line[0] in letter:
+                file2.write(line)
             else:  # 缩进六个空格，主程序前空六格
                 file2.write("      " + line)
     return file2
 
-
-# def del_space(lines):
-#     for line in lines:
-#         line = line.strip(" ")
-#         if line.split():
-#             file2.write(line)
-#     return lines
 
 
 if __name__ == '__main__':
